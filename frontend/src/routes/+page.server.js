fdd956752bb5ae06af45084e16ac6ba2b67a4cdd
Florian Reitz <flo@reitz.dev>
2023-03-02 11:29:06 +0100

    better token exp handling

--- frontend/src/routes/+page.server.js
+++ frontend/src/routes/+page.server.js
@@ -9,10 +9,22 @@ export async function load({params}) {
     if (get(displayStore) === null) {
         const accessToken = await getUser(env.DISPLAY_USER, env.DISPLAY_PASSWORD, env.INTERNAL_BACKEND_URL);
         displayStore.set(accessToken['accessToken']);
+    } 
+    let machines = null;
+    try {
+        machines = await getMachines(get(displayStore), env.INTERNAL_BACKEND_URL);
+    } catch (e) {
+        if(e.status === 401) { // Token expired
+            const accessToken = await getUser(env.DISPLAY_USER, env.DISPLAY_PASSWORD, env.INTERNAL_BACKEND_URL);
+            displayStore.set(accessToken['accessToken']);
+            machines = await getMachines(get(displayStore), env.INTERNAL_BACKEND_URL);
+        } else {
+            console.error(e);
+        }
     }
 
     return {
-        machines: getMachines(get(displayStore), env.INTERNAL_BACKEND_URL)
+        machines: machines
     }
 
 }
